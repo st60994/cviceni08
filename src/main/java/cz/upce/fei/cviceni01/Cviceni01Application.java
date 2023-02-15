95b8455c9375ae39dd6f2bceca9b63c16c8bae49
HrubyVaclav <86952465+HrubyVaclav@users.noreply.github.com>
2023-02-15 15:08:28 +0100

    initial commit

---
+++ src/main/java/cz/upce/fei/cviceni01/Cviceni01Application.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.cviceni01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cviceni01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cviceni01Application.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/cviceni01/Cviceni01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.cviceni01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cviceni01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
